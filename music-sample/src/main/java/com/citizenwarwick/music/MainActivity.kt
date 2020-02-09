651629ec665ace11d04f6b6996d130671a2c9982
Ian Warwick <ian.warwick@just-eat.com>
2020-02-09 18:18:21 +0000

    the basics

---
+++ music-sample/src/main/java/com/citizenwarwick/music/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.citizenwarwick.music
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.citizenwarwick.music.example.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
